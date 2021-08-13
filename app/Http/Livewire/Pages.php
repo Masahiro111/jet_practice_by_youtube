59107ae60517d906c2259bd08ac466447027db3f
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-08-13 19:30:49 +0900

    finsihed part 1

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,10 +2,65 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Pages extends Component
 {
+
+    public $modalFormVisible = false;
+    public $slug;
+    public $title;
+    public $content;
+
+    public function create()
+    {
+        $this->validate([
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages', 'slug')],
+            'content' => 'required',
+        ]);
+
+        Page::create($this->modelData());
+        $this->modalFormVisible = false;
+
+        $this->resetVars();
+    }
+
+    public function updatedTitle($value)
+    {
+        $this->generateSlug($value);
+    }
+
+    private function generateSlug($value)
+    {
+        $process1 = str_replace(' ', '-', $value);
+        $process2 = strtolower($process1);
+        $this->slug = $process2;
+    }
+
+    public function resetVars()
+    {
+        $this->title = null;
+        $this->slug = null;
+        $this->content = null;
+    }
+
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+
+    public function modelData()
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+        ];
+    }
+
     public function render()
     {
         return view('livewire.pages');
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,3 +1,64 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="p-6">
+
+    <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
+        <x-jet-button wire:click="createShowModal">
+            {{ __('Create') }}
+        </x-jet-button>
+    </div>
+
+    {{-- Modal Form --}}
+    <x-jet-dialog-modal wire:model="modalFormVisible">
+        <x-slot name="title">
+            {{ __('Save Page') }}
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="my-4">
+                <x-jet-label for="title" value="{{ __('Title') }}" />
+                <x-jet-input id="title" class="block mt-1 w-full" type="text" wire:model.debounce.800ms="title" />
+                @error('title')
+                <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+            <input wire:model="title" />
+
+            <div class="my-4">
+                <x-jet-label for="slug" value="{{ __('Slug') }}" />
+                <div class="mt-1 flex rounded-md shadow-sm">
+                    <span
+                        class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                        http://laravel12.localhost/
+                    </span>
+                    <input type="text" id="slug" wire:model.debounce.800ms="slug"
+                        class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm flex-1 block w-full rounded-r-md" />
+                </div>
+                @error('slug')
+                <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+            <input wire:model="slug" />
+
+            <div class="my-4">
+                <x-jet-label for="content" value="{{ __('Content') }}" />
+                <textarea wire:model.debounce.800ms="content" id="content" rows="3"
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm flex-1 block w-full rounded-r-md "></textarea>
+                @error('content')
+                <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+            <input wire:model="content" />
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+
+            <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                {{ __('Save') }}
+            </x-jet-button>
+
+        </x-slot>
+
+    </x-jet-dialog-modal>
 </div>

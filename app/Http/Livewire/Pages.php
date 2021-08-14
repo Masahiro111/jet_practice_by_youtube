e36c0c10c32e2fa5c58eadbc86ea607b4bd04af8
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-08-14 23:24:38 +0900

    save

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -5,14 +5,57 @@ namespace App\Http\Livewire;
 use App\Models\Page;
 use Illuminate\Validation\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Pages extends Component
 {
 
+    use WithPagination;
+
     public $modalFormVisible = false;
+    public $modelId;
     public $slug;
     public $title;
     public $content;
+
+
+    public function createShowModal()
+    {
+        $this->resetValidation();
+        $this->resetVars();
+        $this->modalFormVisible = true;
+    }
+
+    public function updateShowModal($id)
+    {
+        $this->resetValidation();
+        $this->resetVars();
+        $this->modelId = $id;
+        $this->modalFormVisible = true;
+        $this->loadModel();
+    }
+
+    public function loadModel()
+    {
+        $data = Page::find($this->modelId);
+        $this->title = $data->title;
+        $this->slug = $data->slug;
+        $this->content = $data->content;
+    }
+
+    public function modelData()
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+        ];
+    }
+
+    public function read()
+    {
+        return Page::paginate(5);
+    }
 
     public function create()
     {
@@ -26,6 +69,18 @@ class Pages extends Component
         $this->modalFormVisible = false;
 
         $this->resetVars();
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages', 'slug')],
+            'content' => 'required',
+        ]);
+
+        Page::find($this->modelId)->update($this->modelData());
+        $this->modalFormVisible = false;
     }
 
     public function updatedTitle($value)
@@ -42,27 +97,16 @@ class Pages extends Component
 
     public function resetVars()
     {
+        $this->modelId = null;
         $this->title = null;
         $this->slug = null;
         $this->content = null;
     }
 
-    public function createShowModal()
-    {
-        $this->modalFormVisible = true;
-    }
-
-    public function modelData()
-    {
-        return [
-            'title' => $this->title,
-            'slug' => $this->slug,
-            'content' => $this->content,
-        ];
-    }
-
     public function render()
     {
-        return view('livewire.pages');
+        return view('livewire.pages', [
+            'data' => $this->read(),
+        ]);
     }
 }
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -6,6 +6,72 @@
         </x-jet-button>
     </div>
 
+    {{-- The data table --}}
+    <div class="flex flex-col  px-4 py-3 text-right sm:px-6">
+        <div class=" -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block w-full">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Title
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Link
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Content
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    &nbsp;
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @if ($data->count()) @foreach ($data as $item)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap text-gray-500">
+                                    {{ $item->title }}
+                                </td>
+                                <td class=" px-6 py-4 whitespace-nowrap text-gray-500">
+                                    {{ $item->slug }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-gray-500">
+                                    {{ $item->content }}
+                                </td>
+                                <td class=" px-6 py-4 whitespace-nowrap text-gray-500 text-right">
+                                    <x-jet-button wire:click="updateShowModal({{ $item->id }})">
+                                        {{ __('Update') }}
+                                    </x-jet-button>
+                                    <x-jet-danger-button wire:click="deleteShowModal({{ $item->id }})">
+                                        {{ __('Delete') }}
+                                    </x-jet-danger-button>
+                                </td>
+                            </tr>
+                            @endforeach @else
+                            <tr>
+                                <td class=" px-6 py-4 whitespace-nowrap text-gray-500 text-right" colspan="4">
+                                    No Results Found
+                                </td>
+                            </tr>
+                            @endif
+
+                            <!-- More items... -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="pt-8">
+            {{ $data->links() }}
+        </div>
+    </div>
+
     {{-- Modal Form --}}
     <x-jet-dialog-modal wire:model="modalFormVisible">
         <x-slot name="title">
@@ -54,9 +120,15 @@
                 {{ __('Cancel') }}
             </x-jet-secondary-button>
 
-            <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
-                {{ __('Save') }}
+            @if($modelId)
+            <x-jet-button class="ml-2" wire:click="update" wire:loading.attr="disabled">
+                {{ __('Update') }}
             </x-jet-button>
+            @else
+            <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                {{ __('Create') }}
+            </x-jet-button>
+            @endif
 
         </x-slot>
 
